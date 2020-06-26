48c612520eb7555f24893312f8d1e2647d619587
Fernando Dávalos <fernando.davalos@freudenberg-it.com>
2020-06-25 23:24:53 -0500

    Added basic serverless template. Added getTodos functionality

---
+++ src/lambda/http/getTodos.ts
@@ -0,0 +1,21 @@
+import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import 'source-map-support/register';
+import { getAllTodos } from '../../businessLogic/todos';
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent) : Promise<APIGatewayProxyResult> => {
+
+    console.log('Processing event: ', event)
+
+    const todos = await getAllTodos();
+
+    return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({
+          items: todos
+        })
+      }
+
+}
